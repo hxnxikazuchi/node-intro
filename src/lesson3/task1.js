dca2b1f5da92d9fd2d628e1a6b1cad5ab6b79e6a
hxnxikazuchi <kupzhasarov14@gmail.com>
2024-03-14 19:00:57 +0500

    feat: task 2

--- src/lesson3/task1.js
+++ src/lesson3/task1.js
@@ -1,4 +1,4 @@
-class EventEmitter {
+export default class EventEmitter {
   #listeners = {};
   #createEvent = (eventName, fn, isOnce) => {
     if (this.#listeners[eventName]) {
@@ -82,38 +82,40 @@ function c2() {
   console.log("yet another event occurred!");
 }
 
-myEmitter.on("eventOne", c1); // Register for eventOne
-myEmitter.on("eventOne", c2); // Register for eventOne
+// PLEASE UNCOMMENT THESE WHEN YOU START CHECKING. I COMMENT THEM TO NOT BOTHER WITH IT IN NEXT TASKS
 
-// Register eventOnce for one time execution
-myEmitter.once("eventOnce", () => console.log("eventOnce once fired"));
-myEmitter.once("init", () => console.log("init once fired"));
+// myEmitter.on("eventOne", c1); // Register for eventOne
+// myEmitter.on("eventOne", c2); // Register for eventOne
 
-// Register for 'status' event with parameters
-myEmitter.on("status", (code, msg) => console.log(`Got ${code} and ${msg}`));
+// // Register eventOnce for one time execution
+// myEmitter.once("eventOnce", () => console.log("eventOnce once fired"));
+// myEmitter.once("init", () => console.log("init once fired"));
 
-myEmitter.emit("eventOne");
+// // Register for 'status' event with parameters
+// myEmitter.on("status", (code, msg) => console.log(`Got ${code} and ${msg}`));
 
-// Emit 'eventOnce' -> After this the eventOnce will be
-// removed/unregistered automatically
-myEmitter.emit("eventOnce");
+// myEmitter.emit("eventOne");
 
-myEmitter.emit("eventOne");
-myEmitter.emit("init");
-myEmitter.emit("init"); // Will not be fired
-myEmitter.emit("eventOne");
-myEmitter.emit("status", 200, "ok");
+// // Emit 'eventOnce' -> After this the eventOnce will be
+// // removed/unregistered automatically
+// myEmitter.emit("eventOnce");
 
-// Get listener's count
-console.log(myEmitter.listenerCount("eventOne"));
+// myEmitter.emit("eventOne");
+// myEmitter.emit("init");
+// myEmitter.emit("init"); // Will not be fired
+// myEmitter.emit("eventOne");
+// myEmitter.emit("status", 200, "ok");
 
-// Get array of rawListeners//
-// Event registered with 'once()' will not be available here after the
-// emit has been called
-console.log(myEmitter.rawListeners("eventOne"));
+// // Get listener's count
+// console.log(myEmitter.listenerCount("eventOne"));
 
-// Get listener's count after remove one or all listeners of 'eventOne'
-myEmitter.off("eventOne", c1);
-console.log(myEmitter.listenerCount("eventOne"));
-myEmitter.off("eventOne", c2);
-console.log(myEmitter.listenerCount("eventOne"));
+// // Get array of rawListeners//
+// // Event registered with 'once()' will not be available here after the
+// // emit has been called
+// console.log(myEmitter.rawListeners("eventOne"));
+
+// // Get listener's count after remove one or all listeners of 'eventOne'
+// myEmitter.off("eventOne", c1);
+// console.log(myEmitter.listenerCount("eventOne"));
+// myEmitter.off("eventOne", c2);
+// console.log(myEmitter.listenerCount("eventOne"));
